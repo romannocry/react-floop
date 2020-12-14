d5681e1a2f1d2bb27bb925371ee6ad8a42152068
romannocry <roman.medioni@gmail.com>
2020-12-13 22:41:44 -0500

    go

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
     },
     output: {
         filename: 'bundle.js',
-        //publicPath: '/react-floop/',
+        publicPath: '/',
         path: path.resolve(__dirname, 'docs'),
     },
     plugins: [new HtmlWebpackPlugin({
